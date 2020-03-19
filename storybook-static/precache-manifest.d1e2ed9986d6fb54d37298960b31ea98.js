2c752cf843cd1882205b91ea66ddcee4fa51a542
Nathan Edwards <nathan_edwards83@hotmail.com>
2020-03-19 11:11:53 +1100

    updated storybook build

---
+++ storybook-static/precache-manifest.d1e2ed9986d6fb54d37298960b31ea98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7d2b47d6fc1e22d59b94abf3197554",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2df6c2c95f9e59d1c5c7.bundle.js"
+  },
+  {
+    "url": "runtime~main.2df6c2c95f9e59d1c5c7.bundle.js"
+  },
+  {
+    "revision": "82e742fc1ff843e44a20",
+    "url": "static/css/main.51451e6f.chunk.css"
+  },
+  {
+    "revision": "b4615352962bc42b9fe6",
+    "url": "static/css/vendors~main.6d2c8d95.chunk.css"
+  },
+  {
+    "url": "vendors~main.2df6c2c95f9e59d1c5c7.bundle.js"
+  },
+  {
+    "url": "vendors~main.2df6c2c95f9e59d1c5c7.bundle.js.LICENSE.txt"
+  }
+]);
--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.fe324ab6204163a50185ec97fee23d2b.js"
+  "precache-manifest.d1e2ed9986d6fb54d37298960b31ea98.js"
 );
 
 self.addEventListener('message', (event) => {
